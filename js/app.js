8352078b79f7ba37bfea671546610a83750a76ab
iFieryGod <61666517+iFieryGod@users.noreply.github.com>
2020-04-20 14:01:39 +0200

    added in the jquery and reset the footer

--- js/app.js
+++ js/app.js
@@ -1,46 +1,151 @@
 'use strict'
+let carts = document.querySelectorAll('.cartEle');
 
-let ele1 = document.querySelector('#product');
-let ele2 = document.querySelector('#price');
-let ele3 = document.querySelector('#quantity');
-let ele4 = document.querySelector('#total');
-let ele5 = document.querySelector('#subtitle');
-let ele6 = document.querySelector('#vat');
-let ele7 = document.querySelector('#addCart1');
-let ele8 = document.querySelector('#addCart2');
-let ele9 = document.querySelector('#addCart3');
-let ele10 = document.querySelector('#cartPic');
-let ele11 = document.querySelector('#enquire1');
-let ele12 = document.querySelector('#enquire2');
-let ele13 = document.querySelector('#enquire3');
+let products = [
+  {
+    name: 'Become a Full Stack Web Developer',
+    tag: 'webdeveloper',
+    price: 26000,
+    inCart: 0
+  },
+  {
+    name: 'Become a Front End Web Developer',
+    tag: 'frontend-dev-banner',
+    price: 9000,
+    inCart: 0
+  },
+  {
+    name: 'Become a Java Spring Boot Developer',
+    tag: 'spring-boot-dev',
+    price: 26000,
+    inCart: 0
+  }
+];
 
-ele11.addEventListener('click',function(){
-  console.log('click');
-  ele10.classList.add('d-block');
-})
+for (let i=0; i < carts.length; i++) {
+  carts[i].addEventListener('click', () => {
+    cartNumbers(products[i]);
+    totalCost(products[i])
+  })
+}
 
-ele12.addEventListener('click',function(){
-  console.log('click');
-  ele10.classList.add('d-block');
-})
+function onloadCartNumbers() {
+  let productNumbers = localStorage.getItem('cartNumbers');
+  if(productNumbers){
+    document.querySelector('.mainCart span').textContent = productNumbers;
+  }
+}
 
-ele13.addEventListener('click',function(){
-  console.log('click');
-  ele10.classList.add('d-block');
-})
+function cartNumbers(product) {
+  let productNumbers = localStorage.getItem('cartNumbers');
+  productNumbers = parseInt(productNumbers);
+  if(productNumbers) {
+    localStorage.setItem('cartNumbers', productNumbers + 1);
+    document.querySelector('.mainCart span').textContent = productNumbers + 1;
+  } else {
+    localStorage.setItem('cartNumbers', 1);
+    document.querySelector('.mainCart span').textContent = 1;
+  }
+  setItems(product);
+}
 
+function setItems(product) {
+ let cartItems = localStorage.getItem('productsInCart');
+ cartItems = JSON.parse(cartItems);
+ 
+  if(cartItems != null) {
 
-ele7.addEventListener('click',function(){
-  console.log('click');
-  ele10.classList.add('d-block');
-})
+    if(cartItems[product.tag] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.tag]: product
+      }
+    }
+    cartItems[product.tag].inCart += 1;
+  } else {
+      product.inCart = 1;
+      cartItems = {
+      [product.tag]: product
+      }
+  }
+    localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+}
 
-ele8.addEventListener('click',function(){
-  console.log('click');
-  ele10.classList.add('d-block');
-})
+function totalCost(product) {
+  // console.log('the product price is', product.price);
+  let cartCost = localStorage.getItem('totalCost');
+  
+  console.log(typeof cartCost);
 
-ele9.addEventListener('click',function(){
-  console.log('click');
-  ele10.classList.add('d-block');
-})
+  if(cartCost != null) {
+    cartCost = parseInt(cartCost);
+    localStorage.setItem('totalCost', cartCost + product.price);
+  } else {
+    localStorage.setItem('totalCost', product.price);
+  }
+}
+
+function displayCart(){
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+  let productContainer = document.querySelector('.cart-container');
+  if( cartItems && productContainer ) {
+    productContainer.innerHTML = '';
+    Object.values(cartItems).map(item => {
+      productContainer.innerHTML += `
+      <td class="product"> 
+        <i class="fas fa-times"></i>
+        <img id="webDev" src="images/${item.tag}.png">
+        <span>${item.name}</span>
+      </td>
+      <td class="price">$${item.price},00</td> 
+      <td class="quantity>
+            <i class="fas fa-arrow-left"></i>
+            <span>${item.inCart}</span>
+            <i class="fas fa-arrow-right"></i>
+      </td> 
+      <td class="total">
+      $${item.inCart * item.price},00
+      </td>
+      `
+    })
+    productContainer.innerHTML += `
+    <div class="float-right">
+    <div class="row">
+      <div class="col-sm">
+          <h3 id="text">Cart Totals</h3>
+      </div>  
+      </div> 
+        <div class="row">
+          <div class="col-sm small">
+            Subtitle
+          </div>
+          <span id="subtitle" class="mr-3"></span>
+        </div>
+          <hr>
+          <div class="row">
+            <div class="col-sm small">
+              Vat(15%)
+            </div>
+            <p id="vat"></p>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="col-sm small">
+              Total
+            </div>
+            <p id="total"></p>
+          </div>
+          <br>
+            <div class="row">
+              <button class="btn btn-outline-primary text-center" id="enquire4">
+              <span id="text">Enquire</span> 
+              </button>
+            </div>
+          </div>
+    `
+  }
+}
+
+onloadCartNumbers();
+displayCart();
--- js/new.js
+++ js/new.js
@@ -1,25 +1,4 @@
-'use strict'
 
-let mainlist = document.querySelector('#product');
-let enqBtn = document.querySelector('#enquire4');
-
-
-let sen1 = document.createElement('p');
-let theX = document.createElement('div');
-let btn = document.createElement('button');
-
-enqBtn.addEventListener('click', function(){
-let message = document.createTextNode('Hello');
-btn.setAttribute('id', 'escapeBtn');
-btn.classList.add('close', 'm-3');
-theX.classList.add('float-left', 'mx-3'); 
-btn.appendChild(sen1);
-sen1.appendChild(message);
-theX.appendChild(btn);
-mainlist.appendChild(theX);
-console.log(sen1)
-
-})
 
 
 
